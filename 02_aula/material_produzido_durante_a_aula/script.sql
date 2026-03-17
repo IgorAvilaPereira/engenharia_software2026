d39d57339b98ac760955a99c9e10d1446c7e1265
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2026-03-17 10:49:28 -0300

    atualizando

---
+++ 02_aula/material_produzido_durante_a_aula/script.sql
@@ -0,0 +1,20 @@
+DROP DATABASE IF EXISTS ifnotes;
+
+CREATE DATABASE ifnotes;
+
+\c ifnotes;
+
+CREATE TABLE usuario (
+    id serial primary key,
+    nome character varying(200) not null,
+    email character varying(200) unique,
+    senha character varying(200) not null
+);
+
+CREATE TABLE anotacao (
+    id serial primary key,
+    titulo text not null,
+    texto text not null,
+    data_hora timestamp default current_timestamp,
+    usuario_id integer references usuario (id)
+);
